9c2a5dcf95902fdfb0aeffaa8bcca1da7cab5db3
MuriloDeSouza <murilo.silva@sou.inteli.edu.br>
2023-02-02 11:30:04 -0300

    Entrega da atividade 1

--- SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
+++ SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
@@ -1,9 +1,9 @@
 extends Node # instancia a classe Node2D
 
-var status = 1
-var vscore = 0
-var x = 1.5 
-var y = 1.5 
+var status = 1 #Se for 0 (dragão parado) se for 1 (dragão em movimento)
+var vscore = 0 #ele conta a pontuação conforme o dragão passa pelos obstáculos
+var x = 1.5 #aqui é a base da velocidade do dragão cravado em 1.5 
+var y = 1.5 #aqui o movimento é horizontal do dragão fazendo com que ele suba e desca, (como ja esta cravado em 1.5 ele ja desce normalmente)
 
 # executa essa função ao carregar o jogo
 func _ready():
@@ -17,12 +17,12 @@ func _process(delta):
 	if status == 1: # jogando
 		
 		# movimenta o cenário do fundo
-		$background.position.x -= 1*x
+		$background.position.x -= 2*x
 		if ($background.position.x) < -200:
 			$background.position.x = 600
 			
 		# movimenta as colunas para colisão
-		$columns.position.x -= 2*x
+		$columns.position.x -= 1*x #Aqui eu posso multiplicar a velocidade do dragão alterando o valor entre 1 e infinito
 		if ($columns.position.x) < -550:
 			$columns.position.x = rand_range(0, 350) - 50
 			$columns.position.y = rand_range(0, 400) - 200
@@ -41,11 +41,11 @@ func _process(delta):
 			
 		# se apertou seta para baixo, aumenta o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_down"):
-			$dragon.position.y += 2
+			$dragon.position.y += 4 #sensibilidade do movimento vertical para baixo
 
 		# se apertou seta para cima, diminui o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_up"):
-			$dragon.position.y -= 4
+			$dragon.position.y -= 4 #sensibilidade do movimento vertical para baixo.
 			
 	elif status == 0: # parado
 		
